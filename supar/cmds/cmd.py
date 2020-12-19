3f2d48312e166c49a09ee466515d83b36f9b5469
Zeeshan Sayyed <zeeshansayyed@gmail.com>
2020-12-18 23:37:10 -0500

    Create separate directory for each experiment. Timestamp in log file name.

--- supar/cmds/cmd.py
+++ supar/cmds/cmd.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
+import time
+from pathlib import Path
 
 import torch
 from supar.utils import Config
 from supar.utils.logging import init_logger, logger
 from supar.utils.parallel import init_device
-from pathlib import Path
-import pudb
+
 
 def parse(parser):
     parser.add_argument('--conf', '-c', help='path to config file')
@@ -16,6 +17,7 @@ def parse(parser):
     parser.add_argument('--batch-size', default=5000, type=int, help='batch size')
     parser.add_argument("--local_rank", type=int, default=-1, help='node rank for distributed training')
     parser.add_argument("--patience", type=int, default=100, help='Patience for early stopping')
+
     args, unknown = parser.parse_known_args()
     args, _ = parser.parse_known_args(unknown, args)
     args = Config(**vars(args))
@@ -27,14 +29,20 @@ def parse(parser):
     args.path = Path(args.path)
     if args.mode == 'train':
         args.path.mkdir(exist_ok=True)
+        args.exp_dir = args.path
+        args.path = args.path / 'best.model'
     else:
-        if not args.path.is_file:
+        if args.path.is_file:
+            args.exp_dir = args.path.parent
+        else:
             raise Exception(f"Cannot {args.mode} as no such file exists")
 
     torch.set_num_threads(args.threads)
     torch.manual_seed(args.seed)
     init_device(args.device, args.local_rank)
-    init_logger(logger, args.path / f"{args.path.stem}.{args.mode}.log")
+    ts = time.localtime()
+    ts = time.strftime("%Y-%m-%d--%H:%M:%S", ts)
+    init_logger(logger, args.exp_dir / f"{args.mode}-{ts}.log")
     logger.info('\n' + str(args))
 
     if args.mode == 'train':
